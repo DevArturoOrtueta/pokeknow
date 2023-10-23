f04d9c805bea192c5f6ef03f0419189c0d438574
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-10-22 21:12:55 -0500

    add abilities, forms and game indices

--- src/slices/pokemonSlice.js
+++ src/slices/pokemonSlice.js
@@ -7,14 +7,29 @@ export const pokemonSlice = createSlice({
         height: 0,
         weight: 0,
         id: 0,
+        abilities: [],
+        forms: [],
+        game_indices: [],
     },
     reducers: {
         findPokemon: (state, action) => {
-            const {name, height, weight, id} = action.payload
+            const {
+                name,
+                height, 
+                weight, 
+                id, 
+                abilities, 
+                forms, 
+                game_indices
+            } = action.payload
+            
             state.name = name;
             state.height = height; //Necesario transformar de hectogramos a kilogramos o libras
             state.weight = weight; //Necesario transformar de decimetros a centimetros o metros
             state.id = id;
+            state.abilities = abilities;
+            state.forms = forms;
+            state.game_indices = game_indices;
         }
     }
 })
