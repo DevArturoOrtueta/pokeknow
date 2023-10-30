dadbf02854227a3ccc8f93d82a283d2b2cc604ad
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-10-30 16:08:01 -0500

    Update

--- src/PokeknowApp.jsx
+++ src/PokeknowApp.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Button} from "@nextui-org/react";
 import { FindPokemon } from './helper/FindPokemon';
+import { PokemonCard } from './components/PokemonCard/PokemonCard';
 
 
 export const PokeknowApp = () => {
