40735cb8d4b5ee9a536228dc7dde7dccdd630213
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-11-01 11:36:14 -0500

    Trying cards

--- src/PokeknowApp.jsx
+++ src/PokeknowApp.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {Button} from "@nextui-org/react";
 import { FindPokemon } from './helper/FindPokemon';
 import { PokemonCard } from './components/PokemonCard/PokemonCard';
+import { useDispatch, useSelector } from 'react-redux';
+import { findPokemon } from './slices/pokemonSlice';
 
 
 export const PokeknowApp = () => {
 
-  return <>
-      <Button color="primary" onClick={FindPokemon()}>
-        Button
-      </Button>
-  </> 
-  
+  const pokemon = useSelector((state) => state.pokemon)
+  const dispatch = useDispatch()
 
+  useEffect(
+    () => {
+      fetch('https://pokeapi.co/api/v2/pokemon/1/')
+      .then((response)=> response.json())
+      .then((data)=> dispatch(findPokemon(data)))
+      .catch((error)=> console.log(error))
+    },
+    []
+  )
+
+  return <PokemonCard {...pokemon}/>
+  
+  
 }
